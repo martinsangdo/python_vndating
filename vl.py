f438dd21662109dfe227c72516ca75c05cdf5cd9
sangdo <sang.do@knorex.com>
2023-09-23 10:39:02 +0700

    - get correlation appearance of numbers

--- vl.py
+++ vl.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 import requests
 
 #######################
-PREVIOUS_RANGE_NUM = 2  #scrape X latest items
-NEXT_RANGE_NUM = 0     #scrape X next items
+PREVIOUS_RANGE_NUM = 5  #scrape X latest items
+NEXT_RANGE_NUM = 5     #scrape X next items
 #######################
 def request_vietlott(DrawId):
     post_url = 'https://vietlott.vn/ajaxpro/Vietlott.PlugIn.WebParts.Game655ResultDetailWebPart,Vietlott.PlugIn.WebParts.ashx'
@@ -31,7 +31,7 @@ def request_vietlott(DrawId):
     return RetExtraParam1
 #######################
 def find_latest_record(db_collection):
-    latest_record = db_collection.find({}).sort("index").limit(1)
+    latest_record = db_collection.find({}).sort("index", -1).limit(1)
     if latest_record is not None:
         return latest_record.next()['index']
     return 0
@@ -70,40 +70,109 @@ def parse_and_save(db_collection, int_index, str_latest_index, text):
 #######################
 def convert_int_to_drawId(int_index):
     str_index = str(int_index)
-    if index < 10:
+    if int_index < 10:
         str_index = '0000' + str(int_index)
-    elif index < 100:
+    elif int_index < 100:
         str_index = '000' + str(int_index)
-    elif index < 1000:
+    elif int_index < 1000:
         str_index = '00' + str(int_index)
     else:
         str_index = '0' + str(int_index)
 
     return str_index
+#######################
+def parse_data(db_collection):
+    int_index = find_latest_record(db_collection)
+    for index in range(int_index - PREVIOUS_RANGE_NUM, int_index + NEXT_RANGE_NUM):
+        str_current_index = convert_int_to_drawId(index);
+        RetExtraParam1 = request_vietlott(str_current_index)
+        if len(RetExtraParam1) > 0:
+            #had data, parse it
+            parse_and_save(db_collection, index, str_current_index, RetExtraParam1)
+        print('finish parsing index: ' + str_current_index)
+####################### calculate frequency of numbers
+def sum_up_statistic(db_collection):
+    records = db_collection.find({})    #find all
+    number_set = {}
+    for record in records:
+        nums = record['nums']
+        for num in nums:
+            if num not in number_set:
+                number_set[num] = 1
+            else:
+                number_set[num] += 1
+    print('statistic ---------- ')
+    for num in number_set:
+        print(str(num) + ',' + str(number_set[num]))
+####################### calculate frequency of numbers at the column index
+def sum_up_statistic_posit(db_collection, column_index):
+    records = db_collection.find({})    #find all
+    number_set = {}
+    for record in records:
+        num = record['nums'][column_index]
+        if num not in number_set:
+            number_set[num] = 1
+        else:
+            number_set[num] += 1
+    print('statistic ---------- ')
+    for num in number_set:
+        print(num)
+    print('---------- ')
+    for num in number_set:
+        print(number_set[num])
+####################### calculate correlation of appearance between of numbers
+# pair(x, y) = Z means: Z times x & y appear together in 1 day
+#
+def calculate_correlation(db_collection):
+    records = db_collection.find({})    #find all
+    pairs = []
+    date_pairs = []
+    #init 2-dimensional array
+    for x in range(0, 55):  #from 1 to 55
+        pairs.append([])
+        date_pairs.append([])
+        for y in range(0, 55):
+            pairs[x].append(0)
+            date_pairs[x].append('')
+    #
+    for record in records:
+        nums = record['nums']
+        for x in range(0, 7):
+            if (x+1) < 7:
+                pairs[nums[x]-1][nums[x+1]-1] += 1
+                if pairs[nums[x]-1][nums[x+1]-1] == 1:
+                    date_pairs[nums[x]-1][nums[x+1]-1] = record['index']
+                else:
+                    date_pairs[nums[x]-1][nums[x+1]-1] = ''
+    print_pairs(pairs)
+    # print(date_pairs[53][0])
+#######################
+def print_pairs(pairs):
+    str_col = ""
+    for col in range(0, 55):
+        if col < 10:
+            str_col += "  " + str(col+1)
+        else:
+            str_col += " " + str(col+1)
+    print("    " + str_col)
+    print("-------")
+    for row in range(0, 55):
+        str_col = ""
+        for col in range(0, 55):
+            if pairs[row][col] < 10:
+                str_col += "  " + str(pairs[row][col])
+            else:
+                str_col += " " + str(pairs[row][col])
+        if (row < 9):
+            print(str(row+1) + ":  " + str_col)
+        else:
+            print(str(row+1) + ": " + str_col)
 ####################### main flow
 client = MongoClient('localhost:27017')
 db_client = client['martin_projects']   #database
 db_collection = db_client['vietlott_655']
 
-int_index = find_latest_record(db_collection)
-for index in range(int_index - PREVIOUS_RANGE_NUM, int_index + NEXT_RANGE_NUM):
-    str_current_index = convert_int_to_drawId(index);
-    RetExtraParam1 = request_vietlott(str_current_index)
-    if len(RetExtraParam1) > 0:
-        #had data, parse it
-        parse_and_save(db_collection, index, str_current_index, RetExtraParam1)
-        print('finish parsing index: ' + str_current_index)
-# print(latest_index)
-# ####################### test
-#
-# org_str = "<div class=\"porlet border CssLength2\">                      <div class=\"header\">                          <div class=\"header-title text-center border-bottom padding_top_30 padding_bottom_20\">                              <div class=\"chitietketqua_title\">                                  <img  src=\"https://media.vietlott.vn//main/06.2018/cms/game/Power655.png\" alt=\"Power 6/55\">                                  <h4>Kết quả quay số mở thưởng POWER 6/55</h4>  \t\t                          <h5>Kỳ quay thưởng <b>#00001</b> ngày <b>01/08/2017</b></h5>                              </div><!-- /chitietketqua_title -->                          </div>                          <!-- /.header-title -->                          <div class=\"header-tool\">                          </div>                          <!-- /.header-tool -->                      </div>                      <!-- /.header -->                      <div class=\"content\">                          <div class=\"day_so_ket_qua border-bottom\">                            <div class=\"day_so_ket_qua_v2\">  \t\t\t                    <span class=\"bong_tron small\">05</span><span class=\"bong_tron small\">10</span><span class=\"bong_tron small\">14</span><span class=\"bong_tron small\">23</span><span class=\"bong_tron small\">24</span><span class=\"bong_tron small\">38</span><i>|</i><span class=\"bong_tron small no-margin-right active\">35</span>                            </div>                           <div class=\"btn_chuyendulieu\">    <a class=\"btn_chuyendulieu_left tat-nutchuyen\" title=\"Đang là kỳ đầu tiên không có kết quả kỳ trước.\"><i class=\"fas fa-angle-left\"></i></a>    <a class=\"btn_chuyendulieu_right\" href=\"javascript:ClientDrawResult('00002');\" title=\"\"><i class=\"fas fa-angle-right\"></i></a>     </div><!-- /btn_chuyendulieu -->                          </div><!-- /day_so_ket_qua -->                      </div>                      <!-- /.content -->                      <div class=\"footer\">                          <div class=\"box_kqtt_nd_chinh text-center\">                              <p class=\"box_kqtt_nd_note color_red\">\"Các con số dự thưởng phải trùng với số kết quả nhưng không cần theo đúng thứ  tự\"</p>                              <p>Truyền Hình Trực Tiếp Trên Kênh VTC3</p>                              <h5>Từ 18h00 – 18h30 Thứ 3 - Thứ 5 - Thứ 7</h5>                          </div><!-- /box_kqtt_nd_chinh -->                      </div>                      <!-- /.footer -->                  </div>                  <!-- /.porlet -->"
-#
-# tree = html.fromstring(org_str)
-#
-# rows = tree.xpath('//span[@class="bong_tron small"]')
-#
-# print(rows)
-# numb = len(rows)
-#
-# for row in rows:
-#     print(row.text)
+# parse_data(db_collection)
+# sum_up_statistic(db_collection)
+# sum_up_statistic_posit(db_collection, 1)
+calculate_correlation(db_collection)
